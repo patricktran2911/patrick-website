c29046083b97b663409fb978049e50f6188082dd
patricktran2911 <nguyenphuctran@csus.edu>
2025-04-30 14:22:55 -0700

    fix resume sizing

--- src/app/resume/render.tsx
+++ src/app/resume/render.tsx
@@ -8,8 +8,8 @@ const sectionVariants = {
 
 export default function Resume() {
   return (
-    <div className="flex-col bg-none text-gray-900 h-full font-sans text-base sm:text-sm md:text-base lg:text-lg xl:text-xl">
-      <header className="relative bg-transparent text-white pt-2 pb-10 px-8 text-center overflow-hidden">
+    <div className="flex flex-col bg-none text-gray-900 h-full w-full font-sans">
+      <header className="bg-transparent text-white pt-2 pb-10 px-8 text-center overflow-hidden">
         <motion.h1
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
@@ -28,7 +28,7 @@ export default function Resume() {
         </motion.p>
       </header>
 
-      <main className="flex-row bg-gray-300 hover:opacity-100 opacity-90 px-8 py-10 max-w-5xl mx-auto space-y-16 transition-opacity duration-300">
+      <main className="flex justify-between space-x-5 bg-gray-300 hover:opacity-100 opacity-90 px-8 py-4 w-fit h-full mx-auto transition-opacity duration-300">
         <motion.section
           initial="hidden"
           whileInView="visible"
@@ -36,14 +36,13 @@ export default function Resume() {
           variants={sectionVariants}
           transition={{ duration: 0.6 }}
           layout={false}
+          className="flex"
         >
-          <h2 className="text-3xl font-bold mb-4">Resume Preview</h2>
-
           <iframe
-            src="https://drive.google.com/file/d/1i_hw3HGp1htJ259xTeN7e6mxywjAXfAw/preview"
-            className="w-full h-[40vh] md:h-[50vh] lg:h-[60vh] border rounded-lg shadow-lg"
+            src="./Assets/pdf/Patrick's-Resume-v2.pdf#zoom=FitH"
+            className="w-auto lg:w-xl lg:h-auto grow border rounded-lg shadow-lg"
             title="Patrick Tran Resume Preview"
-            allow="autoplay"
+            content="application/pdf"
           ></iframe>
         </motion.section>
 
@@ -54,10 +53,10 @@ export default function Resume() {
           variants={sectionVariants}
           transition={{ duration: 0.6, delay: 0.2 }}
         >
-          <h2 className="text-3xl font-bold mb-4">Download</h2>
+          <h2 className="text-xl text-right font-bold mb-4">Download</h2>
           <motion.a
             href="https://drive.google.com/uc?export=download&id=1i_hw3HGp1htJ259xTeN7e6mxywjAXfAw"
-            className="inline-block px-6 py-3 mt-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
+            className="inline-block px-3 py-3 mt-4 bg-blue-600 text-white text-sm rounded-lg hover:bg-blue-700 transition duration-200"
             download
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
